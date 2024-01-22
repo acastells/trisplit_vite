4fdbf94397e99e4b2e275d17a4e6019ffe943d44
acastells <36441239+acastells@users.noreply.github.com>
2024-01-22 19:37:49 +0100

    add members table on detail

--- src/subcomponents/MembersTable.component.tsx
+++ src/subcomponents/MembersTable.component.tsx
@@ -1,5 +1,71 @@
-interface Props {}
+import {
+	Button,
+	Grid,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableRow,
+	TextField,
+} from "@mui/material";
+import React from "react";
+import { TrisplitContext } from "../core/trisplitContext";
+import { Member } from "../vm/vm";
 
-export const MembersTable = (props: Props) => {
-	return <p>MembersTable {props.toString()}</p>;
+export const MembersTable = () => {
+	const { trisplit, setMembers } = React.useContext(TrisplitContext);
+	const [newMember, setNewMember] = React.useState<Member>({ id: Date.now(), name: "" });
+
+	const addNewMember = () => {
+		if (newMember.name === "") {
+			alert("The member must have a name");
+			return;
+		}
+		setMembers([...trisplit.members, newMember]);
+		setNewMember({ id: Date.now(), name: "" });
+	};
+
+	const handleNewMemberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setNewMember({ ...newMember, name: e.target.value });
+	};
+
+	return (
+		<>
+			<p>
+				<b>Members</b>
+			</p>
+			<TableContainer component={Paper}>
+				<Table>
+					<TableBody>
+						<TableRow>
+							<TableCell>
+								<Grid container flexDirection={"column"}>
+									<TextField
+										value={newMember.name}
+										onChange={handleNewMemberChange}
+										InputProps={{
+											endAdornment: (
+												<Button
+													variant="contained"
+													color="primary"
+													onClick={addNewMember}>
+													+
+												</Button>
+											),
+										}}
+									/>
+								</Grid>
+							</TableCell>
+						</TableRow>
+						{trisplit.members.map((row) => (
+							<TableRow key={row.id}>
+								<TableCell>{row.name}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</>
+	);
 };
